c35e5472971141f24bfcd6ea53be73cefe719025
Ryle Macaraig <macaraig@sheridancollege.ca>
2020-10-20 22:28:27 -0400

    created files for appropriate activities

---
+++ app/src/main/java/sheridan/macaraig/mid_v3_p1/InputActivity.kt
@@ -0,0 +1,11 @@
+package sheridan.macaraig.mid_v3_p1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
---
+++ app/src/main/java/sheridan/macaraig/mid_v3_p1/model/BreedInfo.kt
@@ -0,0 +1,4 @@
+package sheridan.macaraig.mid_v3_p1.model
+
+class BreedInfo {
+}
